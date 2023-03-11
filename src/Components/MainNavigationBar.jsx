960964e305629e9bef85119a8295be5a7113cd82
Luana Madeira <105006518+Luanarm@users.noreply.github.com>
2023-03-11 16:28:32 -0500

    added styling to navbar

--- src/Components/MainNavigationBar.jsx
+++ src/Components/MainNavigationBar.jsx
@@ -5,16 +5,21 @@ import { useState } from 'react';
 function MainNavigationBar() {
     const [value, setValue] = useState(0);
     const navigate=useNavigate();
+    const navStyle = {
+        backgroundColor: "#D9EFFF",
+        justifyContent: "right"
+    };
 
     return(
         <BottomNavigation showLabels value={value}
         onChange={(event, newValue) => {
          setValue(newValue);
         }}>
-            <BottomNavigationAction label="Home" onClick={()=>navigate("/")}></BottomNavigationAction>
-            <BottomNavigationAction label="Bouyancy Battery" onClick={()=>navigate("/buoyInfo")}></BottomNavigationAction>
-            <BottomNavigationAction label="Idea" onClick={()=>navigate("/idea")}></BottomNavigationAction>
-            <BottomNavigationAction label="About Us" onClick={()=>navigate("/aboutus")}></BottomNavigationAction>
+            <BottomNavigationAction style={{color: "#14496F"}} icon={<img height="200px" width="200px"src="Images/logo.png"/>}></BottomNavigationAction>
+            <BottomNavigationAction style={{color: "#14496F"}} label="Home" onClick={()=>navigate("/")}></BottomNavigationAction>
+            <BottomNavigationAction style={{color: "#14496F"}} label="Bouyancy Battery" onClick={()=>navigate("/buoyInfo")}></BottomNavigationAction>
+            <BottomNavigationAction style={{color: "#14496F"}} label="Idea" onClick={()=>navigate("/idea")}></BottomNavigationAction>
+            <BottomNavigationAction style={{color: "#14496F"}} label="About Us" onClick={()=>navigate("/aboutus")}></BottomNavigationAction>
         </BottomNavigation>
     )
 }
