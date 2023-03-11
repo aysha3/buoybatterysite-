75daf278381160b7d91edfd9936f08f99f14160f
Daniel Woohyun Kim <dkim67@my.yorku.ca>
2023-03-11 16:18:31 -0500

    Attempted some aboutus changes

--- src/Pages/AboutUsPage.jsx
+++ src/Pages/AboutUsPage.jsx
@@ -1,6 +1,7 @@
 import testImg from "../Images/squareTest.png"
 import AboutUsJson from "../JSONS/AboutUs.json"
 import HeaderParagraphImgCombo from "../Components/HeaderParagraphImgCombo"
+import "../Styles/AboutUs.css";
 
 let counter = 0;
 
@@ -8,18 +9,15 @@ function AboutUsPage() {
 
     const sectionStyle= {
         width: "100%",
-        display: "flex"
     };
 
     const profileStyle= {
-        width: "45%"
+        width: "40%"
     };
 
     return(
         <div id="imgSection" style={sectionStyle}>
-            <div id="profile" style={profileStyle}>
-                <HeaderParagraphImgCombo inputJson={AboutUsJson}></HeaderParagraphImgCombo>
-            </div>
+            <HeaderParagraphImgCombo class="profile" inputJson={AboutUsJson}></HeaderParagraphImgCombo>
         </div>
     )
 }
