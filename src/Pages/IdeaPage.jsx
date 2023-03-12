3ed4ef724754093f89e563b9365ae46bd385b6fc
Daniel Woohyun Kim <dkim67@my.yorku.ca>
2023-03-11 20:12:32 -0500

    Edited image

--- src/Pages/IdeaPage.jsx
+++ src/Pages/IdeaPage.jsx
@@ -1,25 +1,20 @@
-import ReasonforSolnJson from "../JSONS/ReasonforSoln.json"
+import TimelineJson from "../JSONS/TimelineParagraph.json"
 
 function IdeaPage() {
     let Logo = require('../Images/ImplementatationTime.png');
     return(
         <div id="mainMenu">
-            <div id="solution">
-                <h1>Why our Solution is Better</h1>
-                {
-                    ReasonforSolnJson.map ( info => {
-                        return(
-                            <div id="reasons">
-                                <h3>{info.header}</h3>
-                                <p>{info.paragraph}</p>
+            {
+                TimelineJson.map ( info => {
+                    return(
+                        <div>
+                            <img src={Logo} alt="TimeLine" />
+                            <p>{info.paragraph}</p>
                             </div>
-                        )
-                    })
-                }
-                <img src={Logo} alt="TimeLine" />
-            </div>
+                    )
+                })
+            }
         </div>
-        
     )
 }
 export default IdeaPage
